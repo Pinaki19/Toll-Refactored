eb80b18ee80aea80007bfc23e93e767995091c21
Pinaki19 <pinakibanerjee056@gmail.com>
2024-06-15 23:58:22 +0530

    Restructuring for better Authentication and database functions

--- Toll-Plaza/app.py
+++ Toll-Plaza/app.py
@@ -10,24 +10,10 @@ import json
 import io
 import pymongo
 import os
-import pyrebase
 from PIL import Image
-
-
-firebase_config = {
-    'apiKey': "AIzaSyAAJ4Cv2d6cSSbRmnWQPll4kG4TvjdF-W8",
-    'authDomain': "smooth-sailing-ad0d5.firebaseapp.com",
-    'projectId': "smooth-sailing-ad0d5",
-    'storageBucket': "smooth-sailing-ad0d5.appspot.com",
-    'messagingSenderId': "62968749843",
-    'appId': "1:62968749843:web:3bbe8560b1e73c0a3244e6",
-    'measurementId': "G-E6EFBYWRML",
-    'databaseURL': 'None'
-}
-
-firebase = pyrebase.initialize_app(firebase_config)
-auth = firebase.auth()
-
+from utils.helper import *
+from DB_utils.helper import *
+from Auth.helper import *
 
 IST = timezone('Asia/Kolkata')
 
@@ -72,12 +58,6 @@ app.config["MONGO_URI_FOR_GRIDFS"] = os.environ.get('GRIDFS')
 
 mongo_for_gridfs = PyMongo(app, uri=app.config["MONGO_URI_FOR_GRIDFS"])
 fs = gridfs.GridFS(mongo_for_gridfs.db)
-
-
-#----------------------- utility functions ----------------------------------------------------------------------
-
-# ----------------------------------------------------------------------------------------------------------------
-
 
 
 #-------------------------------------------- User Account -------------------------------------------------------
